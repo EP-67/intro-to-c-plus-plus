2b5b14d0e5d0e7dfefba66042f4389ad572701b6
Ethan Pierce <Ethan.pierce@student.manchester.ac.uk>
2026-02-04 11:54:36 +0000

    now succsesfully validates z (checks if input is numerical and below threshold). now need to add input for n and can then use same validation function. look into dealing with input such as 50b

--- Assignement_1.cpp
+++ Assignement_1.cpp
@@ -1,6 +1,6 @@
 #include<iostream>
 
-int validation(int max, int atomic_n)
+/*int validation(int max, int atomic_n, bool test)
 {
     while(std::cin.fail())
     {
@@ -8,6 +8,7 @@ int validation(int max, int atomic_n)
         std::cin.clear();
         std::cin.ignore();
         std::cin>>atomic_n;
+        break;
     }
     while(atomic_n > max)
     {
@@ -15,16 +16,47 @@ int validation(int max, int atomic_n)
         std::cin.ignore();
         std::cin>>atomic_n;
     }
-    return atomic_n;
+    return atomic_n, test;
 
+}*/
+
+int validation(int max, int atomic_n, bool test)
+{
+    while(test == false)
+  {
+    if(std::cin.fail())
+    {
+        std::cout<<"Invalid input, please enter a valid atomic number";
+        std::cin.clear();
+        std::cin.ignore();
+        std::cin>>atomic_n;
+    }
+    else if (atomic_n > max)
+    {
+        std::cout<<"please enter a value less than "<< max;
+        std::cin.ignore();
+        std::cin>>atomic_n;
+    }
+    else
+    {
+        test = true;
+    }
+
+  }
+  return atomic_n;
 }
+
 
 int main()
 {
     int atomic_n;
     const int max{188};
+    bool test{false};
+   
     std::cout<<"what is the atomic number of the nucleus?";
     std::cin>>atomic_n;
-    atomic_n = validation(max, atomic_n);
-    std::cout<<"The atomic number"<<atomic_n<<std::endl;
+    atomic_n = validation(max, atomic_n, test);
+    std::cout<<"The atomic number is "<<atomic_n<<std::endl;
+    
 }
+
